e88c3301cd936357b0f9ee25fbc3a9e493b1465c
ruhefm <herupurnama09@gmail.com>
2024-04-05 01:11:14 +0700

    first

--- utils/supabase.tsx
+++ utils/supabase.tsx
@@ -1,8 +1,9 @@
+require('dotenv').config();
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dblprfcpaguntuduujif.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRibHByZmNwYWd1bnR1ZHV1amlmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMjI0NzEwNCwiZXhwIjoyMDI3ODIzMTA0fQ.yUHLWNDxCexibwYMgXNu1QRPQwRjaTtQ5O2WugLQIGo';
+const supabaseUrl = process.env.SUPABASEURL;
+const supabaseKey = process.env.SUPABASEKEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
